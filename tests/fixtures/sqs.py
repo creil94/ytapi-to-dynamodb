0ce721a81ad09412b360b80624d61ba18e1d3bc5
Christian Reil <c.reil94@web.de>
2024-01-28 16:58:11 +0100

    Dev (#20)
    * Update deployment.yml
    
    * Bump pytest from 7.4.3 to 8.0.0 (#17)
    
    Bumps [pytest](https://github.com/pytest-dev/pytest) from 7.4.3 to 8.0.0.
    - [Release notes](https://github.com/pytest-dev/pytest/releases)
    - [Changelog](https://github.com/pytest-dev/pytest/blob/main/CHANGELOG.rst)
    - [Commits](https://github.com/pytest-dev/pytest/compare/7.4.3...8.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: pytest
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump moto from 4.2.8 to 5.0.0 (#16)
    
    Bumps [moto](https://github.com/getmoto/moto) from 4.2.8 to 5.0.0.
    - [Release notes](https://github.com/getmoto/moto/releases)
    - [Changelog](https://github.com/getmoto/moto/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/getmoto/moto/compare/4.2.8...5.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: moto
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump aws-lambda-powertools from 2.26.1 to 2.32.0 (#14)
    
    Bumps [aws-lambda-powertools](https://github.com/aws-powertools/powertools-lambda-python) from 2.26.1 to 2.32.0.
    - [Release notes](https://github.com/aws-powertools/powertools-lambda-python/releases)
    - [Changelog](https://github.com/aws-powertools/powertools-lambda-python/blob/develop/CHANGELOG.md)
    - [Commits](https://github.com/aws-powertools/powertools-lambda-python/compare/v2.26.1...v2.32.0)
    
    ---
    updated-dependencies:
    - dependency-name: aws-lambda-powertools
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump boto3 from 1.28.84 to 1.34.29 (#15)
    
    Bumps [boto3](https://github.com/boto/boto3) from 1.28.84 to 1.34.29.
    - [Release notes](https://github.com/boto/boto3/releases)
    - [Changelog](https://github.com/boto/boto3/blob/develop/CHANGELOG.rst)
    - [Commits](https://github.com/boto/boto3/compare/1.28.84...1.34.29)
    
    ---
    updated-dependencies:
    - dependency-name: boto3
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * updated mocking for new moto version
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tests/fixtures/sqs.py
+++ tests/fixtures/sqs.py
@@ -1,13 +1,13 @@
 import os
 import pytest
 import boto3
-from moto import mock_sqs
+from moto import mock_aws
 
 
 @pytest.fixture
 def video_sqs_mock():
     '''Mock for the sqs queue'''
-    with mock_sqs():
+    with mock_aws():
         client = boto3.client("sqs")
         client.create_queue(
             QueueName=os.environ['VIDEO_QUEUE'],
